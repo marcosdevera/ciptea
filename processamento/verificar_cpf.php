cb4b1effbddc6c1a31b0ac30d61461b1d9a93311
Fabiano Santos Silva <94010474+kFabada@users.noreply.github.com>
2024-07-08 09:57:18 -0300

    cadastro

--- processamento/verificar_cpf.php
+++ processamento/verificar_cpf.php
@@ -1,17 +1,15 @@
 <?php
-
 include_once("../classes/conexao.class.php");
 
 try {
     $cpf = str_replace(array(".", "-"), "", $_POST['cpf']);
-     
+
     $pdo = Database::conexao();
     $stmt = $pdo->prepare("SELECT COUNT(*) FROM ciptea.dados_pessoa WHERE vch_cpf = :cpf");
+    $result = $stmt->fetchColumn();
     $stmt->bindParam(':cpf', $cpf);
     $stmt->execute();
-    
-    $result = $stmt->fetchColumn();
-    
+
     if ($result > 0) {
         echo json_encode(['status' => 'error', 'message' => 'CPF já cadastrado. <a href="recuperar_senha.php">Recuperar senha</a>']);
     } else {

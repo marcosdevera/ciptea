a5afb3e732da99d0c8cb4b90589a7fb5a2d9074d
KayaMerlin <kayavictorsantoscorreia@gmail.com>
2024-07-08 10:06:23 -0300

    Update verificar_cpf.php

--- processamento/verificar_cpf.php
+++ processamento/verificar_cpf.php
@@ -1,21 +1,23 @@
 <?php
+
 include_once("../classes/conexao.class.php");
 
 try {
-    $cpf = str_replace(array(".", "-"), "", $_POST['cpf']);
+    $cpf = $_POST['cpf'];
 
     $pdo = Database::conexao();
     $stmt = $pdo->prepare("SELECT COUNT(*) FROM ciptea.dados_pessoa WHERE vch_cpf = :cpf");
-    $result = $stmt->fetchColumn();
     $stmt->bindParam(':cpf', $cpf);
     $stmt->execute();
 
+    $result = $stmt->fetchColumn();
+
     if ($result > 0) {
-        echo json_encode(['status' => 'error', 'message' => 'CPF já cadastrado. <a href="recuperar_senha.php">Recuperar senha</a>']);
+        echo json_encode(['status' => 'error', 'message' => 'CPF já cadastrado.']);
     } else {
         echo json_encode(['status' => 'success']);
     }
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo json_encode(['status' => 'error', 'message' => 'Erro ao conectar ao banco de dados: ' . $e->getMessage()]);
 }
 ?>
